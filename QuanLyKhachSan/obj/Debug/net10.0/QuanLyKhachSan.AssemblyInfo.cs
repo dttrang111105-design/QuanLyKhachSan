2e40c038d98e2282b91126b1183701f40b4f183d
Pham Hong Phong <phamhongphong1503@gmail.com>
2026-07-28 12:02:24 +0700

    sua loi lat vat vs lam giam gia

--- QuanLyKhachSan/obj/Debug/net10.0/QuanLyKhachSan.AssemblyInfo.cs
+++ QuanLyKhachSan/obj/Debug/net10.0/QuanLyKhachSan.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyKhachSan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9571c6cbbfb56f11b4c5d43dbcd1f4a8433d8a0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db310e8fd1dd40bd81b48ddc5d12972e24120339")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyKhachSan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyKhachSan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
